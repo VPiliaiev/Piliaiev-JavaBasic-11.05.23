cbcc376f987a6e85b07f7d7368208dbf9cbbb389
Valerii Piliaiev <133023422+VPiliaiev@users.noreply.github.com>
2023-05-12 12:20:58 +0300

    add name

--- java/com/gmail/valeriipiliaiev/HelloWorld.java
+++ java/com/gmail/valeriipiliaiev/HelloWorld.java
@@ -3,5 +3,6 @@ package com.gmail.valeriipiliaiev;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("HelloWorld!");
+        System.out.println("My name Valerii");
     }
 }
